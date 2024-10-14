42c6f8025bd7e44995ff2fe5317ce592c4f627f1
tuan.dang <tuan.dang@gotit.vn>
2024-10-14 13:39:10 +0700

    init

---
+++ resources/views/datatable/index.blade.php
@@ -0,0 +1,15 @@
+{{-- <link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.css" />  
+<script src="https://cdn.datatables.net/2.1.7/js/dataTables.js"></script> --}}
+
+<table class="datatable-ajax">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table>
---
+++ resources/views/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quick component</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.css" />
+
+</head>
+<body>
+    <h1></h1>
+    
+    <script src="https://cdn.datatables.net/2.1.7/js/dataTables.js"></script>
+</body>
+</html>

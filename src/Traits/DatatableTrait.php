c3ab4f8cb3e67428f8af28ce25f7a16bdf95619b
Tuan Dang <tuan.dang@gotit.vn>
2024-11-03 12:01:05 +0700

    feat: update datatable trait

--- src/Traits/DatatableTrait.php
+++ src/Traits/DatatableTrait.php
@@ -25,21 +25,24 @@ trait DatatableTrait
     /**
      * generateEditBtn
      */
-    public function generateEditBtn(string $route, bool $ajax = false): string
+    public function generateEditBtn(string $route, bool $ajax = false, string $class = null): string
     {
+        $class = $class ? " $class" : '';
         if ($ajax) {
-            return '<a title="Sửa" data-toggle="tooltip" href="javascript:void(0);" data-url="'.$route.'" class="btn btn-outline-info btn-sm btn-icon waves-effect waves-themed edit-button-ajax btn-action-table"><i class="fal fa-pen"></i></a>';
+            return '<a title="Sửa" data-toggle="tooltip" href="javascript:void(0);" 
+            data-url="'.$route.'" class="btn btn-outline-info btn-sm btn-icon edit-button-ajax btn-action-table'.$class.'"><i class="fal fa-pen"></i></a>';
         }
 
-        return '<a title="Sửa" data-toggle="tooltip" href="'.$route.'" class="btn btn-outline-info btn-sm btn-icon waves-effect waves-themed btn-action-table"><i class="fal fa-pen"></i></a>';
+        return '<a title="Sửa" data-toggle="tooltip" href="'.$route.'" class="btn btn-outline-info btn-sm btn-icon btn-action-table'.$class.'"><i class="fal fa-pen"></i></a>';
     }
 
     /**
      * generateDeleteBtn
      */
-    public function generateDeleteBtn(string $route): string
+    public function generateDeleteBtn(string $route, string $class): string
     {
-        return '<a title="Xóa" data-toggle="tooltip" data-method="DELETE"  data-url="'.$route.'" class="btn btn-outline-danger btn-sm btn-icon waves-effect waves-themed btn-delete-confirm btn-action-table"><i class="fal fa-trash-alt"></i></a>';
+        return '<a title="Xóa" data-toggle="tooltip" data-method="DELETE"  data-url="'.$route.'
+        " class="btn btn-outline-danger btn-sm btn-icon btn-delete-confirm btn-action-table'.$class.'"><i class="fal fa-trash-alt"></i></a>';
     }
 
     public function getFieldSearch(string $searchField): ?string
@@ -78,5 +81,22 @@ trait DatatableTrait
                 $query->where($name, $value);
             }
         }
+    }
+
+    /**
+     * applyOrderDatatable
+     * 
+     * @param array $listData array data show in table
+     * @param $items
+     * @return array
+     */
+    public function getResponseDatatable(array $listData, $items)
+    {
+        return [
+            'data' => $listData,
+            'draw' => (int) request()->input('draw'),
+            'recordsFiltered' => $items->total(),
+            'recordsTotal' => $items->total(),
+        ];
     }
 }
